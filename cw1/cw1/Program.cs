20b5809003d3e7216670ffd6655f35a4465acdca
s18840 <s18840@pjwstk.edu.pl>
2020-03-05 13:23:42 +0100

    Third commit

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -5,14 +5,34 @@ using System.Threading.Tasks;
 
 namespace cw1
 {
+    public class Student
+    {
+        public string Imie { get; set; }
+        private string _nazwisko;
+        //prywatne pola dawac z podkreslnikiem
+        public string Nazwisko
+        {
+            get { return _nazwisko; }
+            set
+            {
+                if (value == null) throw new ArgumentException();
+                _nazwisko = value;
+            }
+        }
+    }
     public class Program
     {
         public static async Task Main(string[] args)
         {
+            //nazwy metod i zmiennych z duzej litery
+            //prywatne pola dawac z podkreslnikiem
             //if (args.Length == 0) return;
             //nie zagniezdza nam sie kod
+
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+
             var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pja.edu.pl");
+            var result = await client.GetAsync(url);
             //Task <T>
             // ThreadPool() daje nam to dostep do puli watkow
             //
@@ -30,8 +50,11 @@ namespace cw1
                 Console.WriteLine(m);
             }
 
+            var st = new Student();
+            st.Imie = "Jan";
 
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Koniec");
 
         }
     }
