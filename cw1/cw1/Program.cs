2efad3bb0aa7b90728c7bd21532200432b59a8de
s18840 <s18840@pjwstk.edu.pl>
2020-03-05 12:41:12 +0100

    First commit

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
